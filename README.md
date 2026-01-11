aea98b76fc4f030fccc83bdb157ed443436ab403
Habib <hshabibullah@gmail.com>
2026-01-11 09:31:32 +0530

    Initial commit: Fake News Detection ML project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Fake News Detection System
+
+This project detects fake and real news articles using NLP and Machine Learning.
+
+## Tech Stack
+- Python
+- Scikit-learn
+- TF-IDF
+- Logistic Regression
+- Streamlit
+
+## Accuracy
+98.6%
+
+## How to Run
+1. Train model using notebook
+2. Run web app:
+   streamlit run app.py
---
+++ app.py
@@ -0,0 +1,33 @@
+import pickle
+import re
+import string
+import streamlit as st # type: ignore
+
+# Load saved files
+model = pickle.load(open("fake_news_model.pkl", "rb"))
+tfidf = pickle.load(open("tfidf_vectorizer.pkl", "rb"))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text)
+    text = re.sub(r"\[.*?\]", "", text)
+    text = re.sub(r"\w*\d\w*", "", text)
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+st.title("📰 Fake News Detection System")
+
+news = st.text_area("Enter News Text")
+
+if st.button("Predict"):
+    if news.strip() == "":
+        st.warning("Please enter some text")
+    else:
+        cleaned = clean_text(news)
+        vectorized = tfidf.transform([cleaned])
+        result = model.predict(vectorized)[0]
+
+        if result == 0:
+            st.error("🟥 Fake News")
+        else:
+            st.success("🟩 Real News")
